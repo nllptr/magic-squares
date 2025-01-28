352b3fefe6c9eb1769200f53b933ae7b5236f9dc
Simon Wessel <simon.w.karlsson@gmail.com>
2025-01-28 20:46:00 +0000

    add some squares logic and unit tests

---
+++ MagicSquaresApi.Tests/SquaresTests.cs
@@ -0,0 +1,29 @@
+﻿using MagicSquaresApi;
+using Xunit;
+
+namespace MagicSquaresApi.Tests;
+
+public class SquaresTests
+{
+    [Fact]
+    public void TestNewState()
+    {
+        var squares = new Squares();
+        Assert.Empty(squares.GetAllSquares());
+    }
+
+    [Fact]
+    public void TestAddSquare()
+    {
+        var squares = new Squares();
+        squares.AddSquare("#FF0000");
+        Assert.Single(squares.GetAllSquares());
+    }
+
+    [Fact]
+    public void TestAddInvalidSquare()
+    {
+        var squares = new Squares();
+        Assert.Throws<ArgumentException>(() => squares.AddSquare("invalid"));
+    }
+}
--- MagicSquaresApi/Squares.cs
+++ MagicSquaresApi/Squares.cs
@@ -1,4 +1,4 @@
-using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace MagicSquaresApi
 {
@@ -13,7 +13,26 @@ namespace MagicSquaresApi
 
         public void AddSquare(string hexColor)
         {
-            _squares.Add(hexColor);
+            if (IsValidHexColor(hexColor))
+            {
+                _squares.Add(hexColor);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid hex color code", nameof(hexColor));
+            }
+        }
+
+        private bool IsValidHexColor(string hexColor)
+        {
+            if (string.IsNullOrEmpty(hexColor))
+            {
+                return false;
+            }
+
+            // Regex to match hex color codes like #FFFFFF or #FFF
+            var regex = new Regex("^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$");
+            return regex.IsMatch(hexColor);
         }
     }
 }
